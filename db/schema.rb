8355e1f367e128968462cbea473e2f8246e0630c
miki-2222 <futsuu28@gmail.com>
2026-02-09 22:28:17 +0900

    インデックス設定

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_01_195958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "auths", "users"
