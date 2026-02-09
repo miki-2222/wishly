50d3fa4b2ecdefbe8bcd7a65dd3d79d490652662
miki-2222 <futsuu28@gmail.com>
2026-02-10 05:23:12 +0900

    クライアント修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Deviseのメール送信用とOAuth用のデフォルトURLを設定
+  config.action_mailer.default_url_options = { host: 'wishly-m5on.onrender.com', protocol: 'https' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
